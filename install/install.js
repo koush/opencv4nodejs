2317d15b450f67b90ff97d521995865c11d506c0
Koushik Dutta <koushd@gmail.com>
2021-10-02 15:53:55 -0700

    update

--- install/install.js
+++ install/install.js
@@ -1,4 +1,19 @@
 const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+function buildDisabled(dir) {
+    try {
+        const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, dir, 'package.json')));
+        return packageJson.opencv4nodejs.disableAutoBuild
+    }
+    catch (e) {
+    }
+}
+
+if (buildDisabled('../../')) {
+    process.exit();
+}
 
 const cp = child_process.exec('npm run build', { maxBuffer: Infinity }, function (err, stdout, stderr) {
     const _err = err || stderr
